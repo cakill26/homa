fc454ae348c6097aa0dfc874fe525bc5490466a0
envy26 <113749002+cakill26@users.noreply.github.com>
2023-03-10 16:23:19 +0700

    Add files via upload

---
+++ system/info.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# =========================================
+# Quick Setup | Script Setup Manager
+# Edition : Stable Edition V1.0
+# Auther  : Geo Project
+# (C) Copyright 2022
+# =========================================
+clear
+NC='\e[0m'
+## Foreground
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
+
+echo ""
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "         ${RB} 🚀 ${NC} ${WB}Premium Autoscript By JsPhantom ${NC} ${RB}🚀 ${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "                  ${WB}»»» Info Xray «««${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${GB}✅${NC} ${YB}Xray Vmess Ws Tls : 443${NC}         ${WB}|${NC}  ${GB}✅ ${NC} ${YB}Xray Vless Tcp Xtls : 443${NC}"
+echo -e "  ${GB}✅${NC} ${YB}Xray Vless Ws Tls : 443${NC}         ${WB}|${NC}  ${GB}✅ ${NC} ${YB}Xray Vmess Ws None Tls : 80${NC}"
+echo -e "  ${GB}✅${NC} ${YB}Xray Trojan Ws Tls : 443${NC}        ${WB}|${NC}  ${GB}✅ ${NC} ${YB}Xray Vless Ws None Tls : 80${NC}"
+echo -e "  ${GB}✅${NC} ${YB}Xray Trojan Tcp Tls : 443${NC}       ${WB}|${NC}  ${GB}✅ ${NC} ${YB}Xray Trojan Ws None Tls : 80${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "               ${WB}»»» Info SSH & OpenVPN «««${NC}          "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${GB}✅${NC} ${YB}OpenSSH : 22${NC}                 ${WB}|${NC}  ${GB}✅ ${NC} ${YB}OHP SSH : 8686${NC}"
+echo -e "  ${GB}✅${NC} ${YB}OpenVPN : TCP 1194, UDP 2200${NC} ${WB}|${NC}  ${GB}✅ ${NC} ${YB}OHP OpenVPN : 8787${NC}"
+echo -e "  ${GB}✅${NC} ${YB}OpenVPN SSL : 110${NC}            ${WB}|${NC}  ${GB}✅ ${NC} ${YB}Websocket SSH(HTTP) : 80${NC}"
+echo -e "  ${GB}✅${NC} ${YB}Stunnel4 : 222, 777${NC}          ${WB}|${NC}  ${GB}✅ ${NC} ${YB}Websocket SSL(HTTPS) : 443, 2096${NC}"
+echo -e "  ${GB}✅${NC} ${YB}Dropbear : 143, 109${NC}          ${WB}|${NC}  ${GB}✅ ${NC} ${YB}Websocket OpenVPN : 2097${NC}"
+echo -e "  ${GB}✅${NC} ${YB}OHP Dropbear : 8585${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "           ${WB}»»» YAML Service Information «««${NC}          "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${GB}✅${NC} ${YB}YAML XRAY VMESS WS${NC}"
+echo -e "  ${GB}✅${NC} ${YB}YAML XRAY VLESS WS${NC}"
+echo -e "  ${GB}✅${NC} ${YB}YAML XRAY TROJAN WS${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "            ${WB}»»» Info Sqd, Bdvp, Ngnx «««${NC}                 "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${GB}✅${NC} ${YB}Squid Proxy : 3128, 8000 (limit to IP Server${NC}"
+echo -e "  ${GB}✅${NC} ${YB}Badvpn : 7100, 7200, 7300${NC}"
+echo -e "  ${GB}✅${NC} ${YB}Nginx : 81${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "             ${WB}»»» Server Information «««${NC}                 "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${MB}♦️${NC} ${YB}Timezone                : Asia/Kuala_Lumpur (GMT +8)${NC}"
+echo -e "  ${MB}♦️${NC} ${YB}Fail2Ban                : [ON]${NC}"
+echo -e "  ${MB}♦️${NC} ${YB}Dflate                  : [ON]${NC}"
+echo -e "  ${MB}♦️${NC} ${YB}IPtables                : [ON]${NC}"
+echo -e "  ${MB}♦️${NC} ${YB}Auto-Reboot             : [ON]${NC}"
+echo -e "  ${MB}♦️${NC} ${YB}IPV6                    : [OFF]${NC}"
+echo -e ""
+echo -e "  ${GB}✅${NC} ${YB}Autoreboot On 05.00 GMT +8${NC}"
+echo -e "  ${GB}✅${NC} ${YB}Backup & Restore VPS Data${NC}"
+echo -e "  ${GB}✅${NC} ${YB}Automatic Delete Expired Account${NC}"
+echo -e "  ${GB}✅${NC} ${YB}Bandwith Monitor${NC}"
+echo -e "  ${GB}✅${NC} ${YB}RAM & CPU Monitor${NC}"
+echo -e "  ${GB}✅${NC} ${YB}Check Login User${NC}"
+echo -e "  ${GB}✅${NC} ${YB}Check Created Config${NC}"
+echo -e "  ${GB}✅${NC} ${YB}Automatic Clear Log${NC}"
+echo -e "  ${GB}✅${NC} ${YB}Media Checker${NC}"
+echo -e "  ${GB}✅${NC} ${YB}DNS Changer${NC}"
+echo -e "  ${GB}✅${NC} ${YB}Auto Delete Expired Account${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "           ${WB}»»» Autoscript By JsPhantom «««${NC}             "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
